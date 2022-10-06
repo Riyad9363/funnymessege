09c0a90e54a3a129c7fe2af9f003ce4a8bda80b0
Shahria Hussain Riyad <shahriahussainriyad@Shahrias-MacBook-Air.local>
2022-10-06 18:07:27 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+while True:
+    time.sleep(2)
+    pyautogui.typewrite('You are id is now hacked ')
+    time.sleep(1)
+    pyautogui.press('enter')
+
+#Code By Riyad
+
